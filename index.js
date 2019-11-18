f5cd6167589bbfb0d2d5d7dfaadc62f44bc6823d
Jared <44556250+jaredparker@users.noreply.github.com>
2019-11-18 15:55:45 +0000

    Framework of website

---
+++ index.js
@@ -0,0 +1,61 @@
+// - [ SERVER SETUP ] -
+
+console.log("STARTING SERVER...")
+
+// Import libaries
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const subjects = fs.readdirSync( __dirname + '/content/');
+
+const app = express();
+const server = app.listen(8080, () => console.log("LISTENING FOR REQUESTS")); // Listens on port 3000
+app.use('/static', express.static('public')); // Files the client will see from ./static
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// - [ ROUTING ] -
+
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + '/views/@.html');
+});
+
+app.get( '/:subject/*', (req, res) => {
+
+    if( subjects.indexOf( req.params.subject ) != -1 ){
+        let page, content;
+
+        let subject = req.params.subject;
+        let dir = path.join( __dirname, 'content', subject, ...req.params[0].split('/') );
+
+        if( fs.existsSync(`${dir}/.html`) ){
+            content = fs.readFileSync( `${dir}/.html`, 'utf8' );
+
+        } else if( req.params[0] == '' ){
+            content = '<h1>Welcome!</h1>\n<p>Please select the topic you want to explore or revise in the side menu. 😊</p>';
+
+        }else if( fs.existsSync(`${dir}.html`) ){
+            content = fs.readFileSync( `${dir}.html`, 'utf8' );
+
+        } else {
+            return res.redirect( `/${subject}/` );
+        }
+
+        if( req.params[0] ){ let pages = req.params[0].split('/'); page = `- ${pages[pages.length-1].toUpperCase()}`; }
+        
+        res.render( subject, { page, content } );
+
+    } else { return res.redirect('/'); }
+
+});
+
+app.get('/*', (req, res) => {
+    let request = req.params[0].toLowerCase();
+    if( subjects.indexOf( request ) != -1 ){
+        res.redirect(`./${request}/`);
+    } else {
+        res.redirect(`./`);
+    }
+});
---
+++ public/script.js
@@ -0,0 +1,48 @@
+$(document).ready( function(){
+    // Show active page
+    $(`a[href="${location.pathname}"]`).addClass('active');
+
+    // Dropdown to active page
+    $(`a[href="${location.pathname}"]`).parents('.dropdown').children('.nav').slideToggle(0);
+    
+    $('.dropdown a:not(:only-child)').click( function(){
+        $(this).parent().children('.nav').slideToggle();
+    });
+
+    function transitionPage( href ){
+
+        $('.nav a').removeClass( 'active' );
+        $(`a[href="${href}"]`).addClass('active');
+
+        $.ajax({
+            url: href,
+            success: function( data ){
+                $('.main').fadeOut( 300, () => {
+                    let content = $(data).filter('.main').html();
+                    
+                    $('.main').html(content);
+
+                    $('.main').fadeIn( 300 );
+                });
+            }
+        });
+    }
+
+    $(window).on('popstate', function(){
+       transitionPage( location.href );
+    });
+
+    $('a').click( function(e){
+        let href = $(this).attr('href');
+
+        //if( location.href.split('/').includes(href.split('/')[1]) ){};
+
+        e.preventDefault();
+
+        if( href ){
+            window.history.pushState(null, null, href);
+
+            transitionPage( href );
+        };
+    });
+});
